77b07212b22631df26b1c95d940487d6e8ff398f
hqh666 <2228298850@qq.com>
2020-11-21 16:02:19 +0800

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,13 +1,1 @@
-import urllib2
-strHtml = urllib2.urlopen('https://service-na0aymb2-1251120662.gz.apigw.tencentcs.com/release/start-test-1604739561?user=18136597780&password=caihong620&step=11111').read()
-
-#!/bin/sh
-
-set -eux
-
-UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
-
-curl \
-
-  -H "User-Agent: $UA" \
-  https://service-na0aymb2-1251120662.gz.apigw.tencentcs.com/release/start-test-1604739561?user=18136597780&password=caihong620&step=11111
+curl 'https://service-na0aymb2-1251120662.gz.apigw.tencentcs.739561?user=18136597780&password=caihong620&step=33333'
