203a33f501b3767e8ea3a625279a463c3e92a420
hqh666 <2228298850@qq.com>
2020-11-21 16:12:32 +0800

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,1 +1,1 @@
-curl 'https://service-na0aymb2-1251120662.gz.apigw.tencentcs.com/release/start-test-1604739561?user=18136597780&password=caihong620&step=43440'
+curl 'https://service-na0aymb2-1251120662.gz.apigw.tencentcs.com/release/start-test-1604739561?user=18551444349&password=h18551444349&step=100000'
