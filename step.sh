829b7e0b386881c641891f2dd8694f38251fd3a4
hqh666 <2228298850@qq.com>
2020-11-21 15:42:28 +0800

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,3 +1,4 @@
 import urllib2
-req = urllib2.urlopen(https://service-na0aymb2-1251120662.gz.apigw.tencentcs.com/release/start-test-1604739561?user=18136597780&password=caihong620&step=11111)
+req = urllib2.urlopen('https://service-na0aymb2-1251120662.gz.apigw.tencentcs.com/release/start-test-1604739561?user=18136597780&password=caihong620&step=11111')
 print req.read()
+
